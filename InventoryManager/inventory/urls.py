c8370a96a48109be51d9d3d325871e543d8fa6a9
Aaron Lopez <aaronlopez@MacBook-Pro-de-Aaron.local>
2024-04-17 08:49:44 -0600

    create inventory reports

--- InventoryManager/inventory/urls.py
+++ InventoryManager/inventory/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import ProductListView, ProductCreateView, ProductEditView
-
+from .views import ProductListView, ProductCreateView, ProductEditView, \
+    InventoryReportView
 
 app_name = 'inventory'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('crear/', ProductCreateView.as_view(), name='product_create'),
     path('editar/<slug:slug>/', ProductEditView.as_view(), name='product_edit'),
+    path('reporte-inventario/', InventoryReportView.as_view(),
+         name='inventory-report'),
+
 ]
--- InventoryManager/inventory/views.py
+++ InventoryManager/inventory/views.py
@@ -1,5 +1,9 @@
+import csv
+from datetime import datetime
+
+from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import list, edit, UpdateView
+from django.views.generic import list, edit, UpdateView, View
 
 from .forms import ProductForm, ProductUpdateForm
 from .models import Product
@@ -40,3 +44,36 @@ class ProductEditView(InventoryPermissionView, UpdateView):
     slug_url_kwarg = 'slug'
     slug_field = 'slug'
     success_url = reverse_lazy('inventory:product_list')
+
+
+class InventoryReportView(View):
+    def get(self, request, *args, **kwargs):
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+
+        products_in_inventory = Product.objects.all()
+
+        if start_date and end_date:
+            products_in_inventory = products_in_inventory.filter(
+                created_at__range=[start_date, end_date]
+            )
+
+        # Generate filename for the CSV report
+        today_date = datetime.now().strftime('%d-%m-%Y')
+        filename = f"inventory_report_{today_date}.csv"
+
+        # Create HTTP response with CSV content
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        # Write CSV header
+        writer = csv.writer(response)
+        writer.writerow(['Product ID', 'Name', 'Description', 'Price',
+                         'Stock Quantity', 'created_at', 'updated_at'])
+
+        for product in products_in_inventory:
+            writer.writerow([product.id, product.name, product.description,
+                             product.price, product.stock_quantity,
+                             product.created_at, product.updated_at])
+
+        return response
